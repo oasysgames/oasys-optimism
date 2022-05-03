fdc2bd152abdf6313ab7e0e7006587579d8bf103
hironobu ueno <ueno@doublejump.tokyo>
2022-05-03 09:05:19 +0900

    modify role member

--- packages/contracts/contracts/oasys/L1/token/L1StandardERC721.sol
+++ packages/contracts/contracts/oasys/L1/token/L1StandardERC721.sol
@@ -45,14 +45,15 @@ contract L1StandardERC721 is
     constructor(
         string memory name,
         string memory symbol,
-        string memory baseTokenURI
+        string memory baseTokenURI,
+        address creator
     ) ERC721(name, symbol) {
         _baseTokenURI = baseTokenURI;
 
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(DEFAULT_ADMIN_ROLE, creator);
 
-        _setupRole(MINTER_ROLE, _msgSender());
-        _setupRole(PAUSER_ROLE, _msgSender());
+        _setupRole(MINTER_ROLE, creator);
+        _setupRole(PAUSER_ROLE, creator);
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
@@ -71,7 +72,10 @@ contract L1StandardERC721 is
      * - the caller must have the `MINTER_ROLE`.
      */
     function mint(address to, uint32 tokenId) public virtual {
-        require(hasRole(MINTER_ROLE, _msgSender()), "L1StandardERC721: must have minter role to mint");
+        require(
+            hasRole(MINTER_ROLE, _msgSender()),
+            "L1StandardERC721: must have minter role to mint"
+        );
         _mint(to, tokenId);
     }
 
@@ -85,7 +89,10 @@ contract L1StandardERC721 is
      * - the caller must have the `PAUSER_ROLE`.
      */
     function pause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "L1StandardERC721: must have pauser role to pause");
+        require(
+            hasRole(PAUSER_ROLE, _msgSender()),
+            "L1StandardERC721: must have pauser role to pause"
+        );
         _pause();
     }
 
@@ -99,7 +106,10 @@ contract L1StandardERC721 is
      * - the caller must have the `PAUSER_ROLE`.
      */
     function unpause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "L1StandardERC721: must have pauser role to unpause");
+        require(
+            hasRole(PAUSER_ROLE, _msgSender()),
+            "L1StandardERC721: must have pauser role to unpause"
+        );
         _unpause();
     }
 
