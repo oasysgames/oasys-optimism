2f1904cfbce10262e2bed62291a616734a7dd1ac
hironobu ueno <ueno@doublejump.tokyo>
2022-05-08 22:37:21 +0900

    avoid the L2StandardERC721 burnable

--- packages/contracts/contracts/oasys/L2/token/L2StandardERC721.sol
+++ packages/contracts/contracts/oasys/L2/token/L2StandardERC721.sol
@@ -7,13 +7,12 @@ import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {
     ERC721Enumerable
 } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import { ERC721Burnable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 /**
  * @title L2StandardERC721
  * @dev L2StandardERC721 is the Oasys Standard ERC721 contract bridged from the Hub-Layer.
  */
-contract L2StandardERC721 is IL2StandardERC721, ERC721, ERC721Enumerable, ERC721Burnable {
+contract L2StandardERC721 is IL2StandardERC721, ERC721, ERC721Enumerable {
     /**********************
      * Contract Variables *
      **********************/
