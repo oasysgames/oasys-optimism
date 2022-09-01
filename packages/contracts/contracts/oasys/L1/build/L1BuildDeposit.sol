45478afa6d9ada44dda9c8739bc97356d42278ec
ironbeer <7997273+ironbeer@users.noreply.github.com>
2022-09-01 21:40:46 +0900

    ERC20 support for Verse-Builder

--- packages/contracts/contracts/oasys/L1/build/L1BuildDeposit.sol
+++ packages/contracts/contracts/oasys/L1/build/L1BuildDeposit.sol
@@ -1,32 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
 
+/* External Imports */
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/* Internal Imports */
 import { IAllowlist } from "../../libraries/IAllowlist.sol";
 
 /**
  * @title L1BuildDeposit
- * @dev L1BuildAgent manages OAS deposits required to build the Verse-Layer.
+ * @dev L1BuildAgent manages OAS and ERC20 deposits required to build the Verse-Layer.
  */
 contract L1BuildDeposit {
     /**********************
      * Contract Variables *
      **********************/
 
+    address private constant OAS = address(0);
+
     uint256 public requiredAmount;
     uint256 public lockedBlock;
     address public allowlistAddress;
     address public agentAddress;
+    address[] public allowedTokens;
 
     mapping(address => uint256) private _depositTotal;
-    mapping(address => mapping(address => uint256)) private _depositAmount;
+    mapping(address => mapping(address => mapping(address => uint256))) private _depositAmount;
     mapping(address => uint256) private _buildBlock;
 
     /**********
      * Events *
      **********/
 
-    event Deposit(address indexed builder, address depositer, uint256 amount);
-    event Withdrawal(address indexed builder, address depositer, uint256 amount);
+    event Deposit(address indexed builder, address depositer, address token, uint256 amount);
+    event Withdrawal(address indexed builder, address depositer, address token, uint256 amount);
     event Build(address indexed builder, uint256 block);
 
     /***************
@@ -34,18 +41,21 @@ contract L1BuildDeposit {
      ***************/
 
     /**
-     * @param _requiredAmount Required amount of the OAS token to build the Verse-Layer.
-     * @param _lockedBlock Number of blocks to keep OAS tokens locked since building the Verse-Layer
+     * @param _requiredAmount Required amount of token to build the Verse-Layer.
+     * @param _lockedBlock Number of blocks to keep tokens locked since building the Verse-Layer
      * @param _allowlist Address of the Allowlist contract.
+     * @param _allowedTokens Address list of ERC20 tokens that allow deposits.
      */
     constructor(
         uint256 _requiredAmount,
         uint256 _lockedBlock,
-        address _allowlist
+        address _allowlist,
+        address[] memory _allowedTokens
     ) {
         requiredAmount = _requiredAmount;
         lockedBlock = _lockedBlock;
         allowlistAddress = _allowlist;
+        allowedTokens = _allowedTokens;
     }
 
     /********************
@@ -66,18 +76,33 @@ contract L1BuildDeposit {
      * @param _builder Address of the Verse-Builder.
      */
     function deposit(address _builder) external payable {
-        require(_builder != address(0), "builder is zero address");
-        require(msg.value > 0, "no OAS");
-        require(IAllowlist(allowlistAddress).containsAddress(_builder), "builder not allowed");
-
         address depositer = msg.sender;
         uint256 amount = msg.value;
-        require(_depositTotal[_builder] + amount <= requiredAmount, "over deposit amount");
+        _deposit(_builder, depositer, OAS, amount);
 
-        _depositTotal[_builder] += amount;
-        _depositAmount[_builder][depositer] += amount;
+        emit Deposit(_builder, depositer, OAS, amount);
+    }
 
-        emit Deposit(_builder, depositer, amount);
+    /**
+     * Deposits the ERC20 token for the Verse-Builder.
+     * @param _builder Address of the Verse-Builder.
+     * @param _token Address of the ERC20 token.
+     * @param _amount Amount of the ERC20 token.
+     */
+    function depositERC20(
+        address _builder,
+        address _token,
+        uint256 _amount
+    ) external {
+        require(_contains(allowedTokens, _token), "ERC20 not allowed");
+
+        address depositer = msg.sender;
+        _deposit(_builder, depositer, _token, _amount);
+
+        bool success = IERC20(_token).transferFrom(depositer, address(this), _amount);
+        require(success, "ERC20 transfer failed");
+
+        emit Deposit(_builder, depositer, _token, _amount);
     }
 
     /**
@@ -86,20 +111,35 @@ contract L1BuildDeposit {
      * @param _amount Amount of the OAS token.
      */
     function withdraw(address _builder, uint256 _amount) external {
-        require(_builder != address(0), "builder is zero address");
-        require(_amount > 0, "amount is zero");
-
         address depositer = msg.sender;
-        uint256 buildBlock = _buildBlock[_builder];
-        require(buildBlock == 0 || buildBlock + lockedBlock < block.number, "while OAS locked");
-        require(_depositAmount[_builder][depositer] >= _amount, "your deposit amount shortage");
+        _withdraw(_builder, depositer, OAS, _amount);
 
-        _depositTotal[_builder] -= _amount;
-        _depositAmount[_builder][depositer] -= _amount;
         (bool success, ) = depositer.call{ value: _amount }("");
         require(success, "OAS transfer failed");
 
-        emit Withdrawal(_builder, depositer, _amount);
+        emit Withdrawal(_builder, depositer, OAS, _amount);
+    }
+
+    /**
+     * Withdraw the ERC20 token deposited by myself.
+     * @param _builder Address of the Verse-Builder.
+     * @param _token Address of the ERC20 token.
+     * @param _amount Amount of the ERC20 token.
+     */
+    function withdrawERC20(
+        address _builder,
+        address _token,
+        uint256 _amount
+    ) external {
+        require(_contains(allowedTokens, _token), "ERC20 not allowed");
+
+        address depositer = msg.sender;
+        _withdraw(_builder, depositer, _token, _amount);
+
+        bool success = IERC20(_token).transfer(depositer, _amount);
+        require(success, "ERC20 transfer failed");
+
+        emit Withdrawal(_builder, depositer, _token, _amount);
     }
 
     /**
@@ -129,14 +169,29 @@ contract L1BuildDeposit {
      * Returns the amount of the OAS tokens by the depositer.
      * @param _builder Address of the Verse-Builder.
      * @param _depositer Address of the depositer.
-     * @return amount Amount of the OAS tokens by the depositer.
+     * @return amount Amount of the tokens by the depositer.
      */
     function getDepositAmount(address _builder, address _depositer)
         external
         view
         returns (uint256)
     {
-        return _depositAmount[_builder][_depositer];
+        return _depositAmount[OAS][_builder][_depositer];
+    }
+
+    /**
+     * Returns the amount of the OAS tokens by the depositer.
+     * @param _builder Address of the Verse-Builder.
+     * @param _depositer Address of the depositer.
+     * @param _token Address of the token.
+     * @return amount Amount of the tokens by the depositer.
+     */
+    function getDepositERC20Amount(
+        address _builder,
+        address _depositer,
+        address _token
+    ) external view returns (uint256) {
+        return _depositAmount[_token][_builder][_depositer];
     }
 
     /**
@@ -146,5 +201,54 @@ contract L1BuildDeposit {
      */
     function getBuildBlock(address _builder) external view returns (uint256) {
         return _buildBlock[_builder];
+    }
+
+    /**********************
+     * Internal Functions *
+     **********************/
+
+    function _deposit(
+        address _builder,
+        address _depositer,
+        address _token,
+        uint256 _amount
+    ) internal {
+        require(_builder != address(0), "builder is zero address");
+        require(IAllowlist(allowlistAddress).containsAddress(_builder), "builder not allowed");
+        require(_amount > 0, "amount is zero");
+        require(_depositTotal[_builder] + _amount <= requiredAmount, "over deposit amount");
+
+        _depositTotal[_builder] += _amount;
+        _depositAmount[_token][_builder][_depositer] += _amount;
+    }
+
+    function _withdraw(
+        address _builder,
+        address _depositer,
+        address _token,
+        uint256 _amount
+    ) internal {
+        require(_builder != address(0), "builder is zero address");
+        require(_amount > 0, "amount is zero");
+
+        uint256 buildBlock = _buildBlock[_builder];
+        require(buildBlock == 0 || buildBlock + lockedBlock < block.number, "while locked");
+        require(
+            _depositAmount[_token][_builder][_depositer] >= _amount,
+            "your deposit amount shortage"
+        );
+
+        _depositTotal[_builder] -= _amount;
+        _depositAmount[_token][_builder][_depositer] -= _amount;
+    }
+
+    function _contains(address[] memory list, address item) internal pure returns (bool) {
+        uint256 length = list.length;
+        for (uint256 i = 0; i < length; i++) {
+            if (list[i] == item) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- packages/contracts/test/contracts/oasys/L1/build/L1BuildDeposit.spec.ts
+++ packages/contracts/test/contracts/oasys/L1/build/L1BuildDeposit.spec.ts
@@ -3,8 +3,10 @@ import { ContractFactory, Contract } from 'ethers'
 import { SignerWithAddress as Account } from '@nomiclabs/hardhat-ethers/signers'
 import { toWei } from 'web3-utils'
 import { expect } from 'chai'
+import { ZERO_ADDRESS } from '../../../../helpers'
 
 const requiredAmount = toWei('100')
+const halfAmount = toWei('50')
 const lockedBlock = 10
 
 describe('L1BuildDeposit', () => {
@@ -16,9 +18,12 @@ describe('L1BuildDeposit', () => {
   let depositer2: Account
 
   let allowlistFactory: ContractFactory
+  let erc20Factory: ContractFactory
   let l1BuildDepositFactory: ContractFactory
 
   let allowlist: Contract
+  let erc20_A: Contract
+  let erc20_B: Contract
   let l1BuildDeposit: Contract
 
   before(async () => {
@@ -30,6 +35,7 @@ describe('L1BuildDeposit', () => {
     depositer2 = accounts[5]
 
     allowlistFactory = await ethers.getContractFactory('Allowlist')
+    erc20Factory = await ethers.getContractFactory('TestERC20')
     l1BuildDepositFactory = await ethers.getContractFactory('L1BuildDeposit')
   })
 
@@ -39,32 +45,33 @@ describe('L1BuildDeposit', () => {
     allowlist = await allowlistFactory.connect(deployer).deploy()
     await allowlist.connect(deployer).addAddress(builder.address)
 
+    erc20_A = await erc20Factory.connect(deployer).deploy()
+    erc20_B = await erc20Factory.connect(deployer).deploy()
+
     l1BuildDeposit = await l1BuildDepositFactory
       .connect(deployer)
-      .deploy(requiredAmount, lockedBlock, allowlist.address)
-    await network.provider.send('hardhat_setStorageAt', [
-      l1BuildDeposit.address,
-      '0x3',
-      ethers.utils.hexZeroPad(agent.address, 32),
-    ])
+      .deploy(requiredAmount, lockedBlock, allowlist.address, [erc20_A.address])
+    await l1BuildDeposit.connect(deployer).initialize(agent.address)
   })
 
   describe('deposit()', () => {
     it('normally', async () => {
       const tx = await l1BuildDeposit.connect(depositer1).deposit(builder.address, { value: requiredAmount })
-      await expect(tx).to.emit(l1BuildDeposit, 'Deposit').withArgs(builder.address, depositer1.address, requiredAmount)
+      await expect(tx)
+        .to.emit(l1BuildDeposit, 'Deposit')
+        .withArgs(builder.address, depositer1.address, ZERO_ADDRESS, requiredAmount)
     })
 
     it('builder is zero address', async () => {
-      const tx = l1BuildDeposit
-        .connect(depositer1)
-        .deposit('0x0000000000000000000000000000000000000000', { value: requiredAmount })
+      const tx = l1BuildDeposit.connect(depositer1).deposit(ZERO_ADDRESS, {
+        value: requiredAmount,
+      })
       await expect(tx).be.revertedWith('builder is zero address')
     })
 
-    it('no OAS', async () => {
-      const tx = l1BuildDeposit.connect(depositer1).deposit(depositer1.address)
-      await expect(tx).be.revertedWith('no OAS')
+    it('amount is zero', async () => {
+      const tx = l1BuildDeposit.connect(depositer1).deposit(builder.address)
+      await expect(tx).be.revertedWith('amount is zero')
     })
 
     it('builder not allowed', async () => {
@@ -75,6 +82,51 @@ describe('L1BuildDeposit', () => {
     it('over deposit amount', async () => {
       const tx = l1BuildDeposit.connect(depositer1).deposit(builder.address, { value: toWei('999') })
       await expect(tx).be.revertedWith('over deposit amount')
+    })
+  })
+
+  describe('depositERC20()', () => {
+    beforeEach(async () => {
+      await erc20_A.connect(deployer).mint(depositer1.address, requiredAmount)
+    })
+
+    it('normally', async () => {
+      await erc20_A.connect(depositer1).approve(l1BuildDeposit.address, requiredAmount)
+
+      const tx = await l1BuildDeposit.connect(depositer1).depositERC20(builder.address, erc20_A.address, requiredAmount)
+      await expect(tx)
+        .to.emit(l1BuildDeposit, 'Deposit')
+        .withArgs(builder.address, depositer1.address, erc20_A.address, requiredAmount)
+    })
+
+    it('builder is zero address', async () => {
+      const tx = l1BuildDeposit.connect(depositer1).depositERC20(ZERO_ADDRESS, erc20_A.address, requiredAmount)
+      await expect(tx).be.revertedWith('builder is zero address')
+    })
+
+    it('amount is zero', async () => {
+      const tx = l1BuildDeposit.connect(depositer1).depositERC20(builder.address, erc20_A.address, 0)
+      await expect(tx).be.revertedWith('amount is zero')
+    })
+
+    it('builder not allowed', async () => {
+      const tx = l1BuildDeposit.connect(depositer1).depositERC20(depositer1.address, erc20_A.address, requiredAmount)
+      await expect(tx).be.revertedWith('builder not allowed')
+    })
+
+    it('over deposit amount', async () => {
+      const tx = l1BuildDeposit.connect(depositer1).depositERC20(builder.address, erc20_A.address, toWei('999'))
+      await expect(tx).be.revertedWith('over deposit amount')
+    })
+
+    it('ERC20 not allowed', async () => {
+      const tx = l1BuildDeposit.connect(depositer1).depositERC20(builder.address, erc20_B.address, requiredAmount)
+      await expect(tx).to.be.revertedWith('ERC20 not allowed')
+    })
+
+    it('ERC20 transfer failed', async () => {
+      const tx = l1BuildDeposit.connect(depositer1).depositERC20(builder.address, erc20_A.address, requiredAmount)
+      await expect(tx).to.be.revertedWith('ERC20: transfer amount exceeds allowance')
     })
   })
 
@@ -90,14 +142,12 @@ describe('L1BuildDeposit', () => {
       const tx = await l1BuildDeposit.connect(depositer1).withdraw(builder.address, requiredAmount)
       await expect(tx)
         .to.emit(l1BuildDeposit, 'Withdrawal')
-        .withArgs(builder.address, depositer1.address, requiredAmount)
+        .withArgs(builder.address, depositer1.address, ZERO_ADDRESS, requiredAmount)
       expect(await depositer1.getBalance()).to.gte(toWei('9999'))
     })
 
     it('builder is zero address', async () => {
-      const tx = l1BuildDeposit
-        .connect(depositer1)
-        .withdraw('0x0000000000000000000000000000000000000000', requiredAmount)
+      const tx = l1BuildDeposit.connect(depositer1).withdraw(ZERO_ADDRESS, requiredAmount)
       await expect(tx).to.be.revertedWith('builder is zero address')
     })
 
@@ -106,12 +156,12 @@ describe('L1BuildDeposit', () => {
       await expect(tx).to.be.revertedWith('amount is zero')
     })
 
-    it('while OAS locked', async () => {
+    it('while locked', async () => {
       await l1BuildDeposit.connect(agent).build(builder.address)
       await network.provider.send('hardhat_mine', ['0x' + (lockedBlock - 1).toString(16)])
 
       const tx = l1BuildDeposit.connect(depositer1).withdraw(builder.address, requiredAmount)
-      await expect(tx).to.be.revertedWith('while OAS locked')
+      await expect(tx).to.be.revertedWith('while locked')
     })
 
     it('immediate withdraw if not built', async () => {
@@ -128,11 +178,91 @@ describe('L1BuildDeposit', () => {
     })
   })
 
-  describe('build()', () => {
+  describe('withdrawERC20()', () => {
+    beforeEach(async () => {
+      await erc20_A.connect(deployer).mint(depositer1.address, requiredAmount)
+      await erc20_A.connect(depositer1).approve(l1BuildDeposit.address, requiredAmount)
+      await l1BuildDeposit.connect(depositer1).depositERC20(builder.address, erc20_A.address, requiredAmount)
+    })
+
     it('normally', async () => {
+      await l1BuildDeposit.connect(agent).build(builder.address)
+      await network.provider.send('hardhat_mine', ['0x' + lockedBlock.toString(16)])
+
+      expect(await erc20_A.balanceOf(depositer1.address)).to.equal('0')
+
+      const tx = await l1BuildDeposit
+        .connect(depositer1)
+        .withdrawERC20(builder.address, erc20_A.address, requiredAmount)
+      await expect(tx)
+        .to.emit(l1BuildDeposit, 'Withdrawal')
+        .withArgs(builder.address, depositer1.address, erc20_A.address, requiredAmount)
+
+      expect(await erc20_A.balanceOf(depositer1.address)).to.equal(requiredAmount)
+    })
+
+    it('builder is zero address', async () => {
+      const tx = l1BuildDeposit.connect(depositer1).withdrawERC20(ZERO_ADDRESS, erc20_A.address, requiredAmount)
+      await expect(tx).to.be.revertedWith('builder is zero address')
+    })
+
+    it('amount is zero', async () => {
+      const tx = l1BuildDeposit.connect(depositer1).withdrawERC20(builder.address, erc20_A.address, 0)
+      await expect(tx).to.be.revertedWith('amount is zero')
+    })
+
+    it('while locked', async () => {
+      await l1BuildDeposit.connect(agent).build(builder.address)
+      await network.provider.send('hardhat_mine', ['0x' + (lockedBlock - 1).toString(16)])
+
+      const tx = l1BuildDeposit.connect(depositer1).withdrawERC20(builder.address, erc20_A.address, requiredAmount)
+      await expect(tx).to.be.revertedWith('while locked')
+    })
+
+    it('immediate withdraw if not built', async () => {
+      expect(await erc20_A.balanceOf(depositer1.address)).to.equal('0')
+      await l1BuildDeposit.connect(depositer1).withdrawERC20(builder.address, erc20_A.address, requiredAmount)
+      expect(await erc20_A.balanceOf(depositer1.address)).to.equal(requiredAmount)
+    })
+
+    it('deposit amount shortage', async () => {
+      await l1BuildDeposit.connect(agent).build(builder.address)
+      await network.provider.send('hardhat_mine', ['0x' + lockedBlock.toString(16)])
+
+      const tx = l1BuildDeposit.connect(depositer1).withdrawERC20(builder.address, erc20_A.address, toWei('999'))
+      await expect(tx).to.be.revertedWith('your deposit amount shortage')
+    })
+
+    it('ERC20 not allowed', async () => {
+      const tx = l1BuildDeposit.connect(depositer1).withdrawERC20(builder.address, erc20_B.address, requiredAmount)
+      await expect(tx).to.be.revertedWith('ERC20 not allowed')
+    })
+  })
+
+  describe('build()', () => {
+    it('when OAS is deposited', async () => {
       await l1BuildDeposit.connect(depositer1).deposit(builder.address, { value: requiredAmount })
       const tx = await l1BuildDeposit.connect(agent).build(builder.address)
-      await expect(tx).to.emit(l1BuildDeposit, 'Build').withArgs(builder.address, 5)
+      await expect(tx).to.emit(l1BuildDeposit, 'Build').withArgs(builder.address, 8)
+    })
+
+    it('when ERC20 is deposited', async () => {
+      await erc20_A.connect(deployer).mint(depositer1.address, requiredAmount)
+      await erc20_A.connect(depositer1).approve(l1BuildDeposit.address, requiredAmount)
+
+      await l1BuildDeposit.connect(depositer1).depositERC20(builder.address, erc20_A.address, requiredAmount)
+      const tx = await l1BuildDeposit.connect(agent).build(builder.address)
+      await expect(tx).to.emit(l1BuildDeposit, 'Build').withArgs(builder.address, 10)
+    })
+
+    it('when OAS and ERC20 is deposited', async () => {
+      await erc20_A.connect(deployer).mint(depositer1.address, halfAmount)
+      await erc20_A.connect(depositer1).approve(l1BuildDeposit.address, halfAmount)
+
+      await l1BuildDeposit.connect(depositer1).deposit(builder.address, { value: halfAmount })
+      await l1BuildDeposit.connect(depositer1).depositERC20(builder.address, erc20_A.address, halfAmount)
+      const tx = await l1BuildDeposit.connect(agent).build(builder.address)
+      await expect(tx).to.emit(l1BuildDeposit, 'Build').withArgs(builder.address, 11)
     })
 
     it('call from non-agent', async () => {
@@ -142,7 +272,7 @@ describe('L1BuildDeposit', () => {
     })
 
     it('deposit amount shortage', async () => {
-      await l1BuildDeposit.connect(depositer1).deposit(builder.address, { value: toWei('1') })
+      await erc20_A.connect(deployer).mint(depositer1.address, halfAmount)
       const tx = l1BuildDeposit.connect(agent).build(builder.address)
       await expect(tx).to.be.revertedWith('deposit amount shortage')
     })
@@ -156,19 +286,71 @@ describe('L1BuildDeposit', () => {
   })
 
   it('getDepositTotal()', async () => {
+    await erc20_A.connect(deployer).mint(depositer1.address, toWei('50'))
+    await erc20_A.connect(deployer).mint(depositer2.address, toWei('50'))
+    await erc20_A.connect(depositer1).approve(l1BuildDeposit.address, toWei('50'))
+    await erc20_A.connect(depositer2).approve(l1BuildDeposit.address, toWei('50'))
+
+    // deposit.
     await l1BuildDeposit.connect(depositer1).deposit(builder.address, { value: toWei('10') })
+    expect(await l1BuildDeposit.getDepositTotal(builder.address)).to.equal(toWei('10'))
+
     await l1BuildDeposit.connect(depositer2).deposit(builder.address, { value: toWei('20') })
     expect(await l1BuildDeposit.getDepositTotal(builder.address)).to.equal(toWei('30'))
+
+    await l1BuildDeposit.connect(depositer1).depositERC20(builder.address, erc20_A.address, toWei('30'))
+    expect(await l1BuildDeposit.getDepositTotal(builder.address)).to.equal(toWei('60'))
+
+    await l1BuildDeposit.connect(depositer2).depositERC20(builder.address, erc20_A.address, toWei('40'))
+    expect(await l1BuildDeposit.getDepositTotal(builder.address)).to.equal(toWei('100'))
+
+    // withdraw.
+    await l1BuildDeposit.connect(depositer1).withdraw(builder.address, toWei('10'))
+    expect(await l1BuildDeposit.getDepositTotal(builder.address)).to.equal(toWei('90'))
+
+    await l1BuildDeposit.connect(depositer2).withdraw(builder.address, toWei('20'))
+    expect(await l1BuildDeposit.getDepositTotal(builder.address)).to.equal(toWei('70'))
+
+    await l1BuildDeposit.connect(depositer1).withdrawERC20(builder.address, erc20_A.address, toWei('30'))
+    expect(await l1BuildDeposit.getDepositTotal(builder.address)).to.equal(toWei('40'))
+
+    await l1BuildDeposit.connect(depositer2).withdrawERC20(builder.address, erc20_A.address, toWei('40'))
+    expect(await l1BuildDeposit.getDepositTotal(builder.address)).to.equal(toWei('0'))
   })
 
-  it('getDepositAmount()', async () => {
+  it('getDepositAmount() and getDepositERC20Amount()', async () => {
+    const check = async (expOAS: string, expERC20: string) => {
+      const actualOAS = await l1BuildDeposit.getDepositAmount(builder.address, depositer1.address)
+      const actualERC20 = await l1BuildDeposit.getDepositERC20Amount(
+        builder.address,
+        depositer1.address,
+        erc20_A.address
+      )
+      expect(actualOAS).to.equal(toWei(expOAS))
+      expect(actualERC20).to.equal(toWei(expERC20))
+    }
+
+    await erc20_A.connect(deployer).mint(depositer1.address, toWei('50'))
+    await erc20_A.connect(depositer1).approve(l1BuildDeposit.address, toWei('50'))
+
+    // deposit.
     await l1BuildDeposit.connect(depositer1).deposit(builder.address, { value: toWei('10') })
-    expect(await l1BuildDeposit.getDepositAmount(builder.address, depositer1.address)).to.equal(toWei('10'))
+    await check('10', '0')
+
+    await l1BuildDeposit.connect(depositer1).depositERC20(builder.address, erc20_A.address, toWei('20'))
+    await check('10', '20')
+
+    // withdraw.
+    await l1BuildDeposit.connect(depositer1).withdraw(builder.address, toWei('10'))
+    await check('0', '20')
+
+    await l1BuildDeposit.connect(depositer1).withdrawERC20(builder.address, erc20_A.address, toWei('20'))
+    await check('0', '0')
   })
 
   it('getBuildBlock()', async () => {
     await l1BuildDeposit.connect(depositer1).deposit(builder.address, { value: requiredAmount })
     await l1BuildDeposit.connect(agent).build(builder.address)
-    expect(await l1BuildDeposit.getBuildBlock(builder.address)).to.equal(5)
+    expect(await l1BuildDeposit.getBuildBlock(builder.address)).to.equal(8)
   })
 })
