0844e3730f9f4f4d34edc8e973bc21236fd71fc0
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2022-12-28 10:58:58 +0900

    use parseEther at deploy:L1:VerseBuild

--- packages/contracts/tasks/local-deploy-l1.ts
+++ packages/contracts/tasks/local-deploy-l1.ts
@@ -2,30 +2,6 @@
 import { task, subtask } from 'hardhat/config'
 import { ethers } from 'ethers'
 
-// Verse builder contracts
-const L1_BUILD_PARAM_ADDRESS = '0x5200000000000000000000000000000000000006'
-const L1_BUILD_DEPOSIT_ADDRESS = '0x5200000000000000000000000000000000000007'
-const L1_BUILD_AGENT_ADDRESS = '0x5200000000000000000000000000000000000008'
-const L1_BUILD_STEP1_ADDRESS = '0x5200000000000000000000000000000000000009'
-const L1_BUILD_STEP2_ADDRESS = '0x520000000000000000000000000000000000000A'
-const L1_BUILD_STEP3_ADDRESS = '0x520000000000000000000000000000000000000B'
-const L1_BUILD_STEP4_ADDRESS = '0x520000000000000000000000000000000000000c'
-const L1_BUILD_ALLOW_LIST_ADDRESS = '0x520000000000000000000000000000000000000D'
-
-// Messaging
-const L1_ERC721_BRIDGE_ADDRESS = '0x8D736Ad22D106dE9Cf50D0D18D571041a47DD333'
-
-// Rollup
-const VERIFIER_INFO_ADDRESS = '0x5200000000000000000000000000000000000003'
-const OASYS_STATE_COMMITMENT_CHAIN_VERIFIER_ADDRESS =
-  '0x5200000000000000000000000000000000000014'
-
-// Token
-const L1_STANDARD_ERC20_FACTORY_ADDRESS =
-  '0x5200000000000000000000000000000000000004'
-const L1_STANDARD_ERC721_FACTORY_ADDRESS =
-  '0x5200000000000000000000000000000000000005'
-
 const SOAS_ADDRESS = '0x5200000000000000000000000000000000000002'
 
 subtask('deploy:L1:VerseBuild').setAction(async (taskArgs, hre) => {
@@ -34,28 +10,28 @@ subtask('deploy:L1:VerseBuild').setAction(async (taskArgs, hre) => {
 
   const l1BuildDepositFactory = await hre.ethers.getContractFactory(
     'L1BuildDeposit'
-  );
+  )
   const l1BuildParamFactory = await hre.ethers.getContractFactory(
     'L1BuildParam'
-  );
+  )
   const l1BuildStep1Factory = await hre.ethers.getContractFactory(
     'L1BuildStep1'
-  );
+  )
   const l1BuildStep2Factory = await hre.ethers.getContractFactory(
     'L1BuildStep2'
-  );
+  )
   const l1BuildStep3Factory = await hre.ethers.getContractFactory(
     'L1BuildStep3'
-  );
+  )
   const l1BuildStep4Factory = await hre.ethers.getContractFactory(
     'L1BuildStep4'
-  );
+  )
   const l1BuildAllowListFactory = await hre.ethers.getContractFactory(
     'Allowlist'
-  );
+  )
   const l1BuildAgentFactory = await hre.ethers.getContractFactory(
     'L1BuildAgent'
-  );
+  )
 
   const allowlist = await l1BuildAllowListFactory.connect(signer).deploy()
   const l1BuildStep1 = await l1BuildStep1Factory.connect(signer).deploy()
@@ -63,15 +39,15 @@ subtask('deploy:L1:VerseBuild').setAction(async (taskArgs, hre) => {
   const l1BuildStep3 = await l1BuildStep3Factory.connect(signer).deploy()
   const l1BuildStep4 = await l1BuildStep4Factory.connect(signer).deploy()
 
+  await allowlist.addAddress(signer.address)
+
   const l1BuildParam = await l1BuildParamFactory
     .connect(signer)
     .deploy(15_000_000, 32, 60_000, 604_800, 12_592_000)
 
   const l1BuildDeposit = await l1BuildDepositFactory
     .connect(signer)
-    .deploy(ethers.utils.parseEther('100'), 10, allowlist.address, [
-      SOAS_ADDRESS,
-    ])
+    .deploy(ethers.utils.parseEther('1'), 10, allowlist.address, [SOAS_ADDRESS])
   const l1BuildAgent = await l1BuildAgentFactory
     .connect(signer)
     .deploy(
@@ -82,76 +58,17 @@ subtask('deploy:L1:VerseBuild').setAction(async (taskArgs, hre) => {
       l1BuildStep3.address,
       l1BuildStep4.address
     )
-  const allowlistCode = await hre.network.provider.send('eth_getCode', [
-    allowlist.address,
-  ])
-  const l1BuildStep1Code = await hre.network.provider.send('eth_getCode', [
-    l1BuildStep1.address,
-  ])
-  const l1BuildStep2Code = await hre.network.provider.send('eth_getCode', [
-    l1BuildStep2.address,
-  ])
-  const l1BuildStep3Code = await hre.network.provider.send('eth_getCode', [
-    l1BuildStep3.address,
-  ])
-  const l1BuildStep4Code = await hre.network.provider.send('eth_getCode', [
-    l1BuildStep4.address,
-  ])
-  const l1BuildParamCode = await hre.network.provider.send('eth_getCode', [
-    l1BuildParam.address,
-  ])
-  const l1BuildDepositCode = await hre.network.provider.send('eth_getCode', [
-    l1BuildDeposit.address,
-  ])
-  const l1BuildAgentCode = await hre.network.provider.send('eth_getCode', [
-    l1BuildAgent.address,
-  ])
 
-  await hre.network.provider.send('hardhat_setCode', [
-    L1_BUILD_ALLOW_LIST_ADDRESS,
-    allowlistCode,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    L1_BUILD_STEP1_ADDRESS,
-    l1BuildStep1Code,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    L1_BUILD_STEP2_ADDRESS,
-    l1BuildStep2Code,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    L1_BUILD_STEP3_ADDRESS,
-    l1BuildStep3Code,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    L1_BUILD_STEP4_ADDRESS,
-    l1BuildStep4Code,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    L1_BUILD_PARAM_ADDRESS,
-    l1BuildParamCode,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    L1_BUILD_DEPOSIT_ADDRESS,
-    l1BuildDepositCode,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    L1_BUILD_AGENT_ADDRESS,
-    l1BuildAgentCode,
-  ])
-
+  console.log(`L1_BUILD_ALLOW_LIST_ADDRESS is deployed to ${allowlist.address}`)
+  console.log(`L1_BUILD_STEP1_ADDRESS is deployed to ${l1BuildStep1.address}`)
+  console.log(`L1_BUILD_STEP2_ADDRESS is deployed to ${l1BuildStep2.address}`)
+  console.log(`L1_BUILD_STEP3_ADDRESS is deployed to ${l1BuildStep3.address}`)
+  console.log(`L1_BUILD_STEP4_ADDRESS is deployed to ${l1BuildStep4.address}`)
+  console.log(`L1_BUILD_PARAM_ADDRESS is deployed to ${l1BuildParam.address}`)
   console.log(
-    `L1_BUILD_ALLOW_LIST_ADDRESS is deployed to ${L1_BUILD_ALLOW_LIST_ADDRESS}`
+    `L1_BUILD_DEPOSIT_ADDRESS is deployed to ${l1BuildDeposit.address}`
   )
-  console.log(`L1_BUILD_STEP1_ADDRESS is deployed to ${L1_BUILD_STEP1_ADDRESS}`)
-  console.log(`L1_BUILD_STEP2_ADDRESS is deployed to ${L1_BUILD_STEP2_ADDRESS}`)
-  console.log(`L1_BUILD_STEP3_ADDRESS is deployed to ${L1_BUILD_STEP3_ADDRESS}`)
-  console.log(`L1_BUILD_STEP4_ADDRESS is deployed to ${L1_BUILD_STEP4_ADDRESS}`)
-  console.log(`L1_BUILD_PARAM_ADDRESS is deployed to ${L1_BUILD_PARAM_ADDRESS}`)
-  console.log(
-    `L1_BUILD_DEPOSIT_ADDRESS is deployed to ${L1_BUILD_DEPOSIT_ADDRESS}`
-  )
-  console.log(`L1_BUILD_AGENT_ADDRESS is deployed to ${L1_BUILD_AGENT_ADDRESS}`)
+  console.log(`L1_BUILD_AGENT_ADDRESS is deployed to ${l1BuildAgent.address}`)
 })
 
 subtask('deploy:L1:messaging').setAction(async (taskArgs, hre) => {
@@ -164,15 +81,8 @@ subtask('deploy:L1:messaging').setAction(async (taskArgs, hre) => {
 
   const l1ERC721Bridge = await l1ERC721BridgeFactory.connect(signer).deploy()
 
-  const l1ERC721BridgeCode = await hre.network.provider.send('eth_getCode', [
-    l1ERC721Bridge.address,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    L1_ERC721_BRIDGE_ADDRESS,
-    l1ERC721BridgeCode,
-  ])
   console.log(
-    `L1_ERC721_BRIDGE_ADDRESS is deployed to ${L1_ERC721_BRIDGE_ADDRESS}`
+    `L1_ERC721_BRIDGE_ADDRESS is deployed to ${l1ERC721Bridge.address}`
   )
 })
 
@@ -190,26 +100,10 @@ subtask('deploy:L1:rollup').setAction(async (taskArgs, hre) => {
     await oasysStateCommitmentChainVerifierFactory.connect(signer).deploy()
   const verifierInfo = await verifierInfoFactory.connect(signer).deploy()
 
-  const oasysStateCommitmentChainVerifierCode = await hre.network.provider.send(
-    'eth_getCode',
-    [oasysStateCommitmentChainVerifier.address]
-  )
-  const verifierInfoCode = await hre.network.provider.send('eth_getCode', [
-    verifierInfo.address,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    OASYS_STATE_COMMITMENT_CHAIN_VERIFIER_ADDRESS,
-    oasysStateCommitmentChainVerifierCode,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    VERIFIER_INFO_ADDRESS,
-    verifierInfoCode,
-  ])
-
   console.log(
-    `OASYS_STATE_COMMITMENT_CHAIN_VERIFIER_ADDRESS is deployed to ${OASYS_STATE_COMMITMENT_CHAIN_VERIFIER_ADDRESS}`
+    `OASYS_STATE_COMMITMENT_CHAIN_VERIFIER_ADDRESS is deployed to ${oasysStateCommitmentChainVerifier.address}`
   )
-  console.log(`VERIFIER_INFO_ADDRESS is deployed to ${VERIFIER_INFO_ADDRESS}`)
+  console.log(`VERIFIER_INFO_ADDRESS is deployed to ${verifierInfo.address}`)
 })
 
 subtask('deploy:L1:token').setAction(async (taskArgs, hre) => {
@@ -228,26 +122,11 @@ subtask('deploy:L1:token').setAction(async (taskArgs, hre) => {
     .connect(signer)
     .deploy()
 
-  const l1StandardERC20Code = await hre.network.provider.send('eth_getCode', [
-    l1StandardERC20.address,
-  ])
-  const l1StandardERC721Code = await hre.network.provider.send('eth_getCode', [
-    l1StandardERC721.address,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    L1_STANDARD_ERC20_FACTORY_ADDRESS,
-    l1StandardERC20Code,
-  ])
-  await hre.network.provider.send('hardhat_setCode', [
-    L1_STANDARD_ERC721_FACTORY_ADDRESS,
-    l1StandardERC721Code,
-  ])
-
   console.log(
-    `L1_STANDARD_ERC20_FACTORY_ADDRESS is deployed to ${L1_STANDARD_ERC20_FACTORY_ADDRESS}`
+    `L1_STANDARD_ERC20_FACTORY_ADDRESS is deployed to ${l1StandardERC20.address}`
   )
   console.log(
-    `L1_STANDARD_ERC721_FACTORY_ADDRESS is deployed to ${L1_STANDARD_ERC721_FACTORY_ADDRESS}`
+    `L1_STANDARD_ERC721_FACTORY_ADDRESS is deployed to ${l1StandardERC721.address}`
   )
 })
 
