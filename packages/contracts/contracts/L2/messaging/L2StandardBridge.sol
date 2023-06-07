60d6d4baebe747ab4c0e77352ef57e501a905af1
ironbeer <7997273+ironbeer@users.noreply.github.com>
2023-06-07 22:32:36 +0900

    Fixed CVE-2022-35915

--- packages/contracts/contracts/L2/messaging/L2StandardBridge.sol
+++ packages/contracts/contracts/L2/messaging/L2StandardBridge.sol
@@ -7,7 +7,7 @@ import { IL1ERC20Bridge } from "../../L1/messaging/IL1ERC20Bridge.sol";
 import { IL2ERC20Bridge } from "./IL2ERC20Bridge.sol";
 
 /* Library Imports */
-import { ERC165Checker } from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import { ERC165Checker } from "@openzeppelin/contracts-4.9.0/utils/introspection/ERC165Checker.sol";
 import { CrossDomainEnabled } from "../../libraries/bridge/CrossDomainEnabled.sol";
 import { Lib_PredeployAddresses } from "../../libraries/constants/Lib_PredeployAddresses.sol";
 
